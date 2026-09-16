0b90be741d5bb7283066f15b3caa51c52a242c18
DevCesarUnica <292807769+DevCesarUnica@users.noreply.github.com>
2026-09-16 10:27:43 -0300

    Adiciona logo do Ultra Facil na secao Sistemas parceiros

--- src/data/sistemas.ts
+++ src/data/sistemas.ts
@@ -36,7 +36,7 @@ export const systemTools: SystemTool[] = [
 // As 5 ferramentas pertencem a 3 marcas/plataformas parceiras exibidas no rodapé da seção.
 export const systemBrands: SystemBrand[] = [
   { slug: 'astor', name: 'Astor', logo: '/images/systems/astor.png' },
-  { slug: 'ultra-facil', name: 'Ultra Fácil' },
+  { slug: 'ultra-facil', name: 'Ultra Fácil', logo: '/images/systems/ultra-facil.png' },
   {
     slug: 'unica-mais',
     name: 'Única Mais',
